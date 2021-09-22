729c43b71adaf5902895c952d5b988b0e3c5361a
Najmul Huda <najmul.myself@gmail.com>
2021-09-22 22:37:46 +0600

    understanding pattern in php

--- pattern.php
+++ pattern.php
@@ -3,16 +3,33 @@
 for($i=1;$i<10;$i++){
     
     echo PHP_EOL;
-    echo "i: {$i}";
+    // echo "i: {$i}";
     
     for($j=0;$j<$i;$j++){
         echo "*";
-        echo "j : {$j}";
+        // echo "j : {$j}";
     }
-    echo "iins: {$i}";
-    echo "jins: {$j}";
+    // echo "iins: {$i}";
+    // echo "jins: {$j}";
 }
-echo PHP_EOL;
-echo "iout: {$i}";
-echo "jout: {$j}";
+// echo PHP_EOL;
+// echo "iout: {$i}";
+// echo "jout: {$j}";
 
+
+/*
+Undersanding Pattern:
+
+first i = 1 then it will go for second loop . 
+
+j = 0 and it matches the condition and will print * then 
+j will increment its number to 1:
+the condition of the second loop will not match the condition ( j=1 === i=1):
+it will go out of the second loop and increment the i number to i = 2;
+
+then second loop will continues and its print * in second line and continues incrementing j value of 0 to 1
+and will print the * for the second time;
+then it will increment its number to j=2 ; then the condition wont match ( j=2 === i=2);
+
+loop continues this way
+ */
