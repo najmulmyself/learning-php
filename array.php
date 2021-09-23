3adf7c65620d875f6e02cda271f385c254554474
Najmul Huda <najmul.myself@gmail.com>
2021-09-23 19:36:37 +0600

    array method

--- array.php
+++ array.php
@@ -7,7 +7,7 @@
 
     for($i = 0; $i < $x ; $i++){
 
-        echo $Array[$i]."\n";
+        // echo $Array[$i]."\n";
     }
 
     // Associative Array
@@ -18,5 +18,32 @@
         "emily" => 21
     );
 
-    echo $Age["peter"]. "\n". $Age["john"]. "\n" . $Age["emily"];
+    // echo $Age["peter"]. "\n". $Age["john"]. "\n" . $Age["emily"];
+
+
+    // traversing associative array
+
+    // we cant find the expected result when traversing typical loop method 
+    // rather we need to use foreach method to traverse associative array
+    
+    foreach($Age as $key => $valaue){
+        echo $key. "=".$valaue."\n";
+    }
+
+    // we can use another method to traverse associative array
+
+    // this is built in function in php array_keys()
+
+    $keys = array_keys($Age);
+    echo PHP_EOL;
+
+    // echo $keys; // we can't echo here to print array;
+    // we need to use print_r to print array
+
+    print_r($keys);
+
+    for($i= 0 ; $i < count($keys) ; $i++){
+        $key = $keys[$i];
+        echo $Age[$key]. "\n";
+    }
 ?>
