c3efbb854f7f135a06ff75476cf77de218c86a51
Najmul Huda <najmul.myself@gmail.com>
2021-09-29 17:32:26 +0600

    constructor

---
+++ oop/constructor.php
@@ -0,0 +1,23 @@
+<?php 
+
+    class Fruit{
+        public $name;
+        public $color;
+        public $price;
+
+        function __construct($name, $color, $price){
+            $this->name = $name;
+            $this->color = $color;
+            $this->price = $price;
+        }
+
+        function get_name(){
+            echo " Price: $this->price  for $this->name"; ;
+        }
+    }
+
+    $apple = new Fruit('Apple' , 'red' , 15);
+
+    echo $apple->get_name();
+
+?>
