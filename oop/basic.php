aaa4dd4b4a2e62efa0ad5011be512eff8bbf1011
Najmul Huda <najmul.myself@gmail.com>
2021-09-29 17:17:07 +0600

    set() get() method

--- oop/basic.php
+++ oop/basic.php
@@ -3,10 +3,22 @@
 class Classname{
     public $name;
 
-    function __construct($name){
-        this->name = $name;
+    // function __construct($name){
+    //     this->name = $name;
+    // }
+    // function welcome(){
+    //     echo "Welcome to " . "this->$name";
+    // }
+
+    function set_name($name){
+        $this->name = $name;
     }
-    function welcome(){
-        echo "Welcome to " . "this->$name";
+    function get_name(){
+        return $this->name;
     }
 }
+
+$testclass = new Classname();
+
+echo $testclass->set_name('Najmul');
+echo $testclass->get_name();
